b5cac899e3af9a8091c778ca3006ec4c572612ed
irene <irenekurien01@gmail.com>
2021-05-24 00:40:49 +0530

    added theme-switcher

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "theme-ui";
+import theme from "@hackclub/theme";
 
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
@@ -7,37 +9,41 @@ import ProfilePage from "./pages/ProfilePage";
 import FeedbackPage from "./pages/FeedbackPage";
 import ApplicationPage from "./pages/ApplicationsPage";
 import AuthApplicationPage from "./pages/auth/Application";
+import ColorSwitcher from "./components/ColorSwitcher";
 
 function App() {
     return (
-        <Switch>
-            <Route path="/" exact>
-                <HomePage />
-            </Route>
-            <Route path="/apply">
-                <ApplicationPage />
-            </Route>
-            <Route path="/events">
-                <EventsPage />
-            </Route>
-            <Route path="/feedback">
-                <FeedbackPage />
-            </Route>
-            <Route path="/auth">
-                <AuthPage />
-            </Route>
-            <Route path="/profile" exact>
-                <ProfilePage />
-            </Route>
-            <Route path="/profile/application" exact>
-                <AuthApplicationPage />
-            </Route>
-            {/* <Route path="/profile/application/:id">
+        <ThemeProvider theme={theme}>
+            <ColorSwitcher />
+            <Switch>
+                <Route path="/" exact>
+                    <HomePage />
+                </Route>
+                <Route path="/apply">
+                    <ApplicationPage />
+                </Route>
+                <Route path="/events">
+                    <EventsPage />
+                </Route>
+                <Route path="/feedback">
+                    <FeedbackPage />
+                </Route>
+                <Route path="/auth">
+                    <AuthPage />
+                </Route>
+                <Route path="/profile" exact>
+                    <ProfilePage />
+                </Route>
+                <Route path="/profile/application" exact>
+                    <AuthApplicationPage />
+                </Route>
+                {/* <Route path="/profile/application/:id">
                 <AuthApplicationPage />
             </Route> */}
-            {/* <Route path="/profile/events"><AuthEventsPage /></Route>
+                {/* <Route path="/profile/events"><AuthEventsPage /></Route>
             <Route path="/profile/feedback"><AuthFeedbackPage /></Route> */}
-        </Switch>
+            </Switch>
+        </ThemeProvider>
     );
 }
 
