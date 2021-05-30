5fe94eb5c9261abeb429d6627dc48ec57f630f78
irene <irenekurien01@gmail.com>
2021-05-30 23:25:45 +0530

    set UI colors

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,7 +1,74 @@
-import AuthForm from "../components/AuthForm";
+import { useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+import { isEmailValid } from "../utils/validiate";
+import AuthContext from "../store/auth-context";
 
 const AuthPage = () => {
-  return <AuthForm />;
+  const history = useHistory();
+
+  const emailInuputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInuputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if (isEmailValid(enteredEmail)) {
+      const formData = new FormData();
+      formData.append("username", enteredEmail);
+      formData.append("password", enteredPassword);
+
+      try {
+        const res = await axios.post(
+          "http://localhost:8000/auth/token/",
+          formData
+        );
+        authCtx.login(res.data.access_token);
+        history.push("/profile/application");
+      } catch (error) {
+        let errorMessage;
+        if (error.response.status === 422)
+          errorMessage = "Please enter a vaild email id or password";
+        else if (error.response.status === 500)
+          errorMessage = "Opps, something went wrong please try again later!";
+        else errorMessage = error.response.data.detail;
+
+        alert(errorMessage);
+      }
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth">
+        <h1>Login</h1>
+        <form onSubmit={submitHandler}>
+          <div className="auth__control">
+            <label htmlFor="email">Your Email</label>
+            <input type="email" id="email" required ref={emailInuputRef} />
+          </div>
+          <div className="auth__control">
+            <label htmlFor="password">Your Password</label>
+            <input
+              type="password"
+              id="password"
+              required
+              ref={passwordInputRef}
+            />
+          </div>
+          <div className="auth__actions">
+            <button className="btn--toggle">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthPage;
