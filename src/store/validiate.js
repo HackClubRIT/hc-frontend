6bd31e44c8d935fc57df1c57e103499ec20310b5
irene <irenekurien01@gmail.com>
2021-06-07 19:39:16 +0530

    created text input component

--- src/store/validiate.js
+++ src/store/validiate.js
@@ -1,14 +1,14 @@
 export function isEmailValid(val) {
-    return new RegExp(
-      "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
-    ).test(val);
-  }
-  
-  export function isEmpty(val) {
-    return val.trim().length === 0;
-  }
-  
-  export function isPhoneNumber(val) {
-    let phoneno = /^\d{10}$/;
-    return (val.test(phoneno));
-  }
+  return new RegExp(
+    "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+  ).test(val);
+}
+
+export function isEmpty(val = "") {
+  return val.trim().length === 0;
+}
+
+export function isPhoneNumber(val) {
+  let phoneno = /^\d{10}$/;
+  return val.test(phoneno);
+}
